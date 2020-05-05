1fdba3f0a544aa89a600f7f4d2612e6fdc08637c
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-05-05 20:57:27 -0300

    Add files via upload

---
+++ for/args.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Andre Augusto Giannotti Scota (https://sites.google.com/view/a2gs/)
+
+# Script exit if a command fails:
+#set -e
+
+# Script exit if a referenced variable is not declared:
+#set -u
+
+# If one command in a pipeline fails, its exit code will be returned as the result of the whole pipeline:
+#set -o pipefail
+
+# Activate tracing:
+#set -x
+
+for args in $*
+do
+  echo  "$args"
+done
