06a3eb83168c31b99350a03919ea05c4e68dc1a1
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-08-21 19:17:06 -0300

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -11,3 +11,13 @@ echo '$RANDOM:' $RANDOM
 echo '$SECONDS:' $SECONDS
 echo '$OSTYPE:' $OSTYPE
 funcName
+
+echo -e'\n----------------------------------\nEnvironment Variables:\n')
+echo -e '$0\tThe filename of the current script.'
+echo -e '$n\tThese variables correspond to the arguments with which a script was invoked. Here n is a positive decimal number corresponding to the position of an argument (the first argument is $1, the second argument is $2, and so on).'
+echo -e '$#\tThe number of arguments supplied to a script.'
+echo -e '$*\tAll the arguments are double quoted. If a script receives two arguments, $* is equivalent to $1 $2.'
+echo -e '$@\tAll the arguments are individually double quoted. If a script receives two arguments, $@ is equivalent to $1 $2.'
+echo -e '$?\tThe exit status of the last command executed.'
+echo -e '$$\tThe process number of the current shell. For shell scripts, this is the process ID under which they are executing.'
+echo -e '$!\tThe process number of the last background command.'
