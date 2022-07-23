301731aabfbd1904298a6725333e8e068e17a6a9
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2022-07-23 14:17:43 -0300

    Update radiolist.sh

--- dialog/radiolist.sh
+++ dialog/radiolist.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-tempFile=`mktemp -p ./`
-
-dialog --backtitle "CPU Selection" --radiolist "Select CPU type:" 10 40 4 1 386SX off 2 386DX off 3 486SX off 4 486DX4 on 5 i3 off 6 i5 off 7 i7 off 2>$tempFile
+opt=$(dialog --backtitle "CPU Selection" --radiolist "Select CPU type:" 10 40 4 1 386SX off 2 386DX off 3 486SX off 4 486DX4 on 5 i3 off 6 i5 off 7 i7 off)
 
 clear
 
-echo "Return: [`cat $tempFile`]"
-
-rm -f $tempFile
+echo "[$opt]"
