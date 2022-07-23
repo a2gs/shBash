cd25ccf9ad63f31bd80889de4cdcd57d445e7a58
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2022-07-23 14:16:49 -0300

    Update checklist.sh

--- dialog/checklist.sh
+++ dialog/checklist.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-tempFile=`mktemp -p ./`
-
-dialog --checklist 'Choose toppings:' 10 40 5 1 'Cheese' on 2 'Tomato Sauce' on 3 'Anchovies' off 4 'Bacon' on 5 'Salad' off 2>$tempFile
+opt=$(dialog --stdout --checklist 'Choose toppings:' 10 40 5 1 'Cheese' on 2 'Tomato Sauce' on 3 'Anchovies' off 4 'Bacon' on 5 'Salad' off)
 
 clear
 
-echo "Return: [`cat $tempFile`]"
-
-rm -f $tempFile
+echo "[$opt]"
