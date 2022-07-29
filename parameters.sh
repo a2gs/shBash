ad0a06e9ab6c508250a59fdf5d233da921b9805e
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2022-07-29 20:19:57 -0300

    Update parameters.sh

--- parameters.sh
+++ parameters.sh
@@ -8,25 +8,27 @@ helpFunction()
    echo "Usage: $0 -a parameterA -b parameterB -c parameterC"
    echo -e "\t-a Description of what is parameterA"
    echo -e "\t-b Description of what is parameterB"
-   echo -e "\t-c Description of what is parameterC"
+   echo -e "\t-c Flag parameterC"
+   echo -e "\t-d Description of what is parameterD"
    exit 1 # Exit script after printing help
 }
 
 echo "Total parameters: $#"
 echo "They are: $@"
 
-while getopts "a:b:c:" opt
+while getopts "a:b:cd:" opt
 do
    case "$opt" in
       a ) parameterA="$OPTARG" ;;
       b ) parameterB="$OPTARG" ;;
-      c ) parameterC="$OPTARG" ;;
+      c ) parameterC=13 ;;
+      d ) parameterD="$OPTARG" ;;
       \? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$parameterA" ] || [ -z "$parameterB" ] || [ -z "$parameterC" ]
+if [ -z "$parameterA" ] || [ -z "$parameterB" ] || [ -z "$parameterD" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -36,3 +38,4 @@ fi
 echo "$parameterA"
 echo "$parameterB"
 echo "$parameterC"
+echo "$parameterD"
